3cb752ced382e264cc02c88c0c4832108d8d64bb
SAE1\1029483 <maocharliemao@gmail.com>
2022-11-18 12:40:33 +1100

    inital commit

---
+++ Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        print("floor is ouch");
+        print("ouch i hit the" + collision.gameObject.name);        
+    }
+
+
+
+}
---
+++ Assets/script/sxript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sxript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+    other.gameObject.GetComponent<Rigidbody>().AddForce(other.gameObject.transform.up * 100);
+    }
+
+}
